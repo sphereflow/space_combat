0b733168a553beb5ccd9572db7b489b1fb6d5ff5
sphereflow <johannprescher@gmx.de>
2013-12-03 12:39:33 +0100

    add 3D vector math functions - speed up contains_vertex_of
    - added functions:
       * dot product
       * length
       * unit
       * An.rotate for rotation without intermediate matrix
    
    - contains_vertex_of now does not call set_matrix_position
      and instead enters it directly

--- math_3d.pyx
+++ math_3d.pyx
@@ -5,7 +5,7 @@ import numpy as np
 import numpy.linalg as npla
 from numpy import matrix
 import math
-from libc.math cimport sin, cos, floor, ceil, M_PI, M_1_PI
+from libc.math cimport sin, cos, floor, ceil, sqrt, M_PI, M_1_PI
 
 DTYPE = np.double
 ctypedef np.double_t DTYPE_t
@@ -25,6 +25,16 @@ cdef class An :
 
    def set_degrees(self, double f) :
       self.val = (f * 2.0 * M_PI / 360.0) % (2.0 * M_PI)
+
+   @cython.boundscheck(False)
+   def rotate(self, np.ndarray[DTYPE_t, ndim = 1] v) :
+      cdef np.ndarray[DTYPE_t, ndim = 1] r = np.zeros(4)
+      cdef double s = sin(self.val)
+      cdef double c = cos(self.val)
+      r[0] = c * v[0] + s * v[1]
+      r[1] = -s * v[0] + c * v[1]
+      r[3] = 1.0
+      return r
 
    def __add__(self, An a) :
       ret = An()
@@ -68,11 +78,28 @@ def set_matrix_position_val(np.ndarray m, double x, double y) :
    mv[3, 1] = y
 
 @cython.boundscheck(False)
-def set_matrix_rotation(np.ndarray m, An a) :
+def set_matrix_rotation(np.ndarray[DTYPE_t, ndim = 2] m, An a) :
    cdef double [:, :] mv = m
    mv[1, 1] = mv[0, 0] = cos(a.val)
    mv[1, 0] = sin(a.val)
    mv[0, 1] = -mv[1, 0]
+
+@cython.boundscheck(False)
+def dot(np.ndarray[DTYPE_t, ndim = 1] v1, np.ndarray[DTYPE_t, ndim = 1] v2) :
+   return v1[0] * v2[0] + v1[1] * v2[1] + v1[2] * v2[2]
+
+@cython.boundscheck(False)
+def length(np.ndarray[DTYPE_t, ndim = 1] v1) :
+   return sqrt(v1[0] ** 2 + v1[1] ** 2 + v1[2] ** 2)
+
+@cython.boundscheck(False)
+@cython.cdivision(True)
+def unit(np.ndarray[DTYPE_t, ndim = 1] v1) :
+   cdef double sql = sqrt(v1[0] ** 2 + v1[1] ** 2 + v1[2] ** 2)
+   if sql == 0.0 :
+      return v1.copy()
+   cdef double f = 1.0 / sql
+   return v1 * f
 
 cdef class Rect :
    mbuf = np.identity(4)
@@ -104,7 +131,8 @@ cdef class Rect :
       if (x_diff ** 2 + y_diff ** 2) > ((rad1 + rad2) ** 2) :
          return False
       set_matrix_rotation(Rect.mrbuf, self.angle * -1)
-      set_matrix_position_val(Rect.mpbuf, self.pos[0] * -1.0, self.pos[1] * -1.0)
+      Rect.mpbuf[3, 0] = -self.pos[0]
+      Rect.mpbuf[3, 1] = -self.pos[1]
       cdef np.ndarray[DTYPE_t, ndim = 2] mv1inv = np.dot(Rect.mpbuf, Rect.mrbuf)
       cdef np.ndarray[DTYPE_t, ndim = 2] mtrans = np.dot(r2.get_modelview(), mv1inv)
       cdef double [:, :] mtransv = mtrans
@@ -112,7 +140,7 @@ cdef class Rect :
       cdef double [:] p2v = p2
       cdef np.ndarray[DTYPE_t, ndim = 1] p = np.zeros(4)
       cdef double [:] pv = p
-      cdef int i
+      cdef int i, j, k
       for i in range(4):
          p2v[3] = 1.0
          #p = np.dot(p2v, mtransv)
@@ -120,8 +148,8 @@ cdef class Rect :
             pv[j] = 0.0
             for k in range(4) :
                pv[j] += p2v[k] * mtransv[k, j]
-         p[3] = 1.0
-         if (abs(p[0]) <= (self.width * 0.5)) and (abs(p[1]) <= (self.height * 0.5)) :
+         pv[3] = 1.0
+         if (abs(pv[0]) <= (self.width * 0.5)) and (abs(pv[1]) <= (self.height * 0.5)) :
             return True
          if i == 0 :
             p2v[0] *= -1.0
