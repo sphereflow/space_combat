8a589cbb5b261bc6e25cd7a5a45afc726900a1ca
sphereflow <johannprescher@gmx.de>
2013-12-13 11:06:59 +0100

    add billboard.pyx to cython build

--- setup.py
+++ setup.py
@@ -3,3 +3,4 @@ from distutils.extension import Extension
 from Cython.Distutils import build_ext
 from Cython.Build import cythonize
 setup(name = 'math_3d', ext_modules = cythonize("math_3d.pyx"),)
+setup(name = 'billboard', ext_modules = cythonize("billboard.pyx"),)
